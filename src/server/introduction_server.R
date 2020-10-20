c99c1eae00288957c348ea81be95934bbbb9edcd
osymandius <bolistevens@gmail.com>
2020-10-20 15:02:35 +0100

    fiddle api key

--- src/server/introduction_server.R
+++ src/server/introduction_server.R
@@ -3,8 +3,8 @@ introduction_server <- function(input, output, session) {
   observeEvent(input$api_button, {
     
     adr_url <<- "https://dev.adr.fjelltopp.org/"
-    # my_adr_key <- input$api_key
-    my_adr_key <<- "c9acd056-2872-4b77-aa56-cfd1a8b34076"
+    my_adr_key <- input$api_key
+    # my_adr_key <<- "c9acd056-2872-4b77-aa56-cfd1a8b34076"
     
     pkg_options <<- package_search(q = "type:naomi-data-package", as = "table",
                                   url = adr_url, key = my_adr_key)
